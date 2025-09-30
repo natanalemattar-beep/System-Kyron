dece2b021783f12a8736925b6245a57354da5f4e
carlos mattar <natanalemattar@gmail.com>
2025-09-30 23:10:06 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,6 +13,7 @@ import {
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
