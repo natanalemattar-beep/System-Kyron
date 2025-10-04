99463ccae058aa47c23ee51d870b75ba09bb494a
carlos mattar <natanalemattar@gmail.com>
2025-10-04 14:50:45 +0000

    cerrado todo para que se vea arreglado y ordenado

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider defaultOpen={true}>
+    <SidebarProvider defaultOpen={false}>
       <AppSidebar />
       <SidebarInset>
          <header className="p-4 flex justify-between items-center border-b sticky top-0 z-10 h-16 px-6 md:px-8 bg-card/80 backdrop-blur-sm">
