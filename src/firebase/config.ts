27495218351998d4db012c48b530b1fbc38a60cf
carlos mattar <natanalemattar@gmail.com>
2025-11-30 22:40:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "fintrack-automation2-534-c4c71",
+  "appId": "1:1082863387183:web:5ddbb80c305bb595043cda",
+  "apiKey": "AIzaSyDMQI7SZQJ-7AzZfhrZMho2LMvI0zonsKY",
+  "authDomain": "fintrack-automation2-534-c4c71.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1082863387183"
+};
