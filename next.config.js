1c5140815b5b5c72fec4c2f5b192ed764a5f9eee
carlos mattar <natanalemattar@gmail.com>
2025-12-16 00:55:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: __dirname,
   experimental: {
   },
-  allowedDevOrigins: ["https://6000-firebase-fintrack-automation2-1759388821350.cluster-f73ibkkuije66wssuontdtbx6q.cloudworkstations.dev"],
+  allowedDevOrigins: ["https://6000-firebase-fintrack-automation2-1759388821350.cluster-f73ibkkuije66wssuontdtbx6q.cloudworkstations.dev", "https://9000-firebase-fintrack-automation2-1759388821350.cluster-f73ibkkuije66wssuontdtbx6q.cloudworkstations.dev"],
   images: {
     unoptimized: true,
     remotePatterns: [
