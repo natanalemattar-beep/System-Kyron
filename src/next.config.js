8b00ddbdecd31799d108caba9f7cf1b76ad5f7d3
carlos mattar <natanalemattar@gmail.com>
2025-12-17 16:30:28 +0000

    en ves de false sea true que funcione pues

---
+++ src/next.config.js
@@ -0,0 +1,36 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  outputFileTracingRoot: __dirname,
+  experimental: {
+  },
+  devIndicators: true,
+  allowedDevOrigins: ["https://6000-firebase-fintrack-automation2-1759388821350.cluster-f73ibkkuije66wssuontdtbx6q.cloudworkstations.dev", "https://9000-firebase-fintrack-automation2-1759388821350.cluster-f73ibkkuije66wssuontdtbx6q.cloudworkstations.dev"],
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+       {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
