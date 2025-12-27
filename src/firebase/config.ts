674a5cbdd3962bc1574724de0ffe586f350bc973
carlos mattar <natanalemattar@gmail.com>
2025-12-27 15:46:16 +0000

    sigue ocurriendo el error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "fintrack-automation2-534-c4c71",
-  "appId": "1:1082863387183:web:5ddbb80c305bb595043cda",
-  "apiKey": "AIzaSyDMQI7SZQJ-7AzZfhrZMho2LMvI0zonsKY",
-  "authDomain": "fintrack-automation2-534-c4c71.firebaseapp.com",
-  "databaseURL": "https://fintrack-automation2-534-c4c71-default-rtdb.firebaseio.com",
-  "measurementId": "",
-  "messagingSenderId": "1082863387183"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
