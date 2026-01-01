22b136dda662ee34ec70f7650dc1b6d686ee341c
carlos mattar <natanalemattar@gmail.com>
2026-01-01 21:49:28 +0000

    Error {name: "Error", environmentName: "Server", constructor: Object} na

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,7 @@
-import { createLocalizedPathnamesNavigation, Pathnames } from 'next-intl/navigation';
- 
+import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {createLocalizedPathnamesNavigation, Pathnames} from 'next-intl/navigation';
+
 export const locales = ['en', 'es'] as const;
 export const defaultLocale = 'es';
 export const localePrefix = 'as-needed';
@@ -7,6 +9,15 @@ export const localePrefix = 'as-needed';
 export const pathnames = {
   '/': '/',
 } satisfies Pathnames<typeof locales>;
+
+export const {Link, redirect, usePathname, useRouter} =
+  createLocalizedPathnamesNavigation({locales, localePrefix, pathnames});
+
+export default getRequestConfig(async ({locale}) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
  
-export const { Link, redirect, usePathname, useRouter } =
-  createLocalizedPathnamesNavigation({ locales, localePrefix, pathnames });
+  return {
+    messages: (await import(`@/messages/${locale}.json`)).default
+  };
+});
