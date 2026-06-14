c0ec845d482dedbff7bd4918900eba216b56f253
Carlos <carlos@system-kyron.com>
2026-06-14 19:57:32 -0400

    fix: replace createLocalizedPathnamesNavigation with createNavigation for next-intl v4 API

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,10 +1,9 @@
-
-import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
+import {createNavigation} from 'next-intl/navigation';
 import {locales, defaultLocale, localePrefix, pathnames} from './config';
 
 export {locales, defaultLocale, localePrefix, pathnames};
 
-export const {Link, redirect, usePathname, useRouter} = createLocalizedPathnamesNavigation({
+export const {Link, redirect, usePathname, useRouter} = createNavigation({
   locales,
   localePrefix,
   pathnames
